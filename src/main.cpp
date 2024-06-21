6c58c62d5196f843da836cc4801b3264bc8b5aff
jasonjasont <114400246+jasonjasont@users.noreply.github.com>
2024-06-21 14:23:08 +0200

    push final

--- src/main.cpp
+++ src/main.cpp
@@ -2,23 +2,26 @@
 #include <Ticker.h>
 #include <WiFi.h>
 
-#include "DHTesp.h" // Click here to get the library: http://librarymanager/All#DHTesp
+#include "DHTesp.h"
+#include <ArduinoJson.h>
+#include <SPIFFS.h>
+
 #define LED_PIN 23
 #define HW512_PIN 22
 
-/** Initialize DHT sensor 1 */
+/** Initialize DHT sensor */
 DHTesp dhtSensor1;
 
 /** Task handle for the light value read task */
 TaskHandle_t tempTaskHandle = NULL;
-/** Pin number for DHT11 1 data pin */
+/** Pin number for DHT11 data pin */
 int dhtPin1 = 25;
 
 /** Ticker for temperature reading */
 Ticker tempTicker;
 /** Flags for temperature readings finished */
 bool gotNewTemperature = false;
-/** Data from sensor 1 */
+/** Data from sensor */
 TempAndHumidity sensor1Data;
 
 /* Flag if main loop is running */
@@ -26,27 +29,32 @@ bool tasksEnabled = false;
 
 bool isLedRed = false;
 unsigned long previousMillis = 0;
-const long interval = 60000; // 30 secondes
+const long interval = 60000; // 30 seconds
 
 // web server
 const char *ssid = "CoursAssembleur";
 const char *password = "CoursAssembleur$*";
+
+// forceled
+bool forceLedOn = false;
 
 WiFiServer serveur(80);
 
 String generateHTMLPage()
 {
   String pageHtml = "<html><body>";
-  pageHtml += "<h1>Données du capteur</h1>";
-  pageHtml += "<p>Température: " + String(sensor1Data.temperature, 2) + " °C</p>";
-  pageHtml += "<p>Humidité: " + String(sensor1Data.humidity, 1) + " %</p>";
+  pageHtml += "<h1>Donnees du capteur</h1>";
+  pageHtml += "<p>Temperature: " + String(sensor1Data.temperature, 2) + " °C</p>";
+  pageHtml += "<p>Humidite: " + String(sensor1Data.humidity, 1) + " %</p>";
   pageHtml += "<form action=\"/eteindre\" method=\"get\"><input type=\"submit\" value=\"Eteindre la LED\"></form>";
+  pageHtml += "<form action=\"/allumer\" method=\"get\"><input type=\"submit\" value=\"Allumer la LED\"></form>";
+  pageHtml += "<form action=\"/lirejson\" method=\"get\"><input type=\"submit\" value=\"Lire JSON\"></form>";
   pageHtml += "</body></html>";
   return pageHtml;
 }
 
 /**
- * Task to reads temperature from DHT11 sensor
+ * Task to read temperature from DHT sensor
  * @param pvParameters
  *		pointer to task parameters
  */
@@ -56,22 +64,14 @@ void tempTask(void *pvParameters)
   while (1) // tempTask loop
   {
     if (tasksEnabled && !gotNewTemperature)
-    { // Read temperature only if old data was processed already
-      // Reading temperature for humidity takes about 250 milliseconds!
-      // Sensor readings may also be up to 2 seconds 'old' (it's a very slow sensor)
-      sensor1Data = dhtSensor1.getTempAndHumidity(); // Read values from sensor 1
-
+    {
+      sensor1Data = dhtSensor1.getTempAndHumidity(); // Read values from sensor
       gotNewTemperature = true;
     }
     vTaskSuspend(NULL);
   }
 }
 
-/**
- * triggerGetTemp
- * Sets flag dhtUpdated to true for handling in loop()
- * called by Ticker tempTicker
- */
 void triggerGetTemp()
 {
   if (tempTaskHandle != NULL)
@@ -80,28 +80,25 @@ void triggerGetTemp()
   }
 }
 
-/**
- * Arduino setup function (called once after boot/reboot)
- */
 void setup()
 {
   Serial.begin(115200);
-  Serial.println("Example for 3 DHT11/22 sensors");
+  Serial.println("Setup for DHT sensor");
   pinMode(LED_PIN, OUTPUT);
-  pinMode(HW512_PIN, OUTPUT); // Configurez la broche HW-512 comme sortie
+  pinMode(HW512_PIN, OUTPUT);
 
-  // Initialize temperature sensor 1
+  // Initialize temperature sensor
   dhtSensor1.setup(dhtPin1, DHTesp::DHT22);
 
   // Start task to get temperature
   xTaskCreatePinnedToCore(
-      tempTask,        /* Function to implement the task */
-      "tempTask ",     /* Name of the task */
-      4000,            /* Stack size in words */
-      NULL,            /* Task input parameter */
-      5,               /* Priority of the task */
-      &tempTaskHandle, /* Task handle. */
-      1);              /* Core where the task should run */
+      tempTask,
+      "tempTask ",
+      4000,
+      NULL,
+      5,
+      &tempTaskHandle,
+      1);
 
   if (tempTaskHandle == NULL)
   {
@@ -109,92 +106,138 @@ void setup()
   }
   else
   {
-    // Start update of environment data every 30 seconds
     tempTicker.attach(30, triggerGetTemp);
   }
 
-  // Start the web server
+  // Start the Wi-Fi and server
   tasksEnabled = true;
   Serial.print("Connection au réseau ");
   Serial.println(ssid);
   WiFi.disconnect();
   WiFi.begin(ssid, password);
 
-  // Tant que l'état n'est pas connecté, attendre...
   while (WiFi.status() != WL_CONNECTED)
   {
     delay(500);
     Serial.print(".");
   }
 
-  // Une fois connecté, on affiche l'adresse IP du ESP32
   Serial.println("");
   Serial.println("Connecté au Wi-fi");
   Serial.println("Adresse IP = ");
   Serial.println(WiFi.localIP());
 
   serveur.begin();
-
-} // End of setup.
+  if (!SPIFFS.begin(true))
+  {
+    Serial.println("An error has occurred while mounting SPIFFS");
+    return;
+  }
+}
 
 void loop()
 {
   if (gotNewTemperature)
   {
-    Serial.println("Données reçues:");
-    Serial.println("Temp: " + String(sensor1Data.temperature, 2) + "'C Humidity: " + String(sensor1Data.humidity, 1) + "%");
+    StaticJsonDocument<200> doc;
+    doc["temperature"] = sensor1Data.temperature;
+    doc["humidity"] = sensor1Data.humidity;
 
-    if (sensor1Data.humidity > 60)
+    File file = SPIFFS.open("/data.json", FILE_WRITE);
+    if (!file)
     {
-      digitalWrite(LED_PIN, HIGH); // Allumez la LED
-                                   /*isLedRed = true;*/
+      Serial.println("There was an error opening the file for writing");
+      return;
     }
-    else
+
+    if (serializeJson(doc, file) == 0)
     {
-      digitalWrite(LED_PIN, LOW); // Éteignez la LED
+      Serial.println("Failed to write to file");
     }
+
+    file.close();
 
     gotNewTemperature = false;
   }
 
   unsigned long currentMillis = millis();
+
   if (isLedRed && currentMillis - previousMillis >= interval)
   {
-    // Sauvegardez la dernière fois où vous avez basculé le HW-512
     previousMillis = currentMillis;
-    digitalWrite(HW512_PIN, HIGH); // Activez le HW-512
-    delay(1000);                   // Laissez le HW-512 activé pendant 1 seconde
-    digitalWrite(HW512_PIN, LOW);  // Désactivez le HW-512
+    digitalWrite(HW512_PIN, HIGH);
+    delay(1000);
+    digitalWrite(HW512_PIN, LOW);
   }
 
-  // Regarde s'il y a un client sur le serveur
   WiFiClient client = serveur.available();
-
   if (client)
   {
     Serial.println("Nouveau Client.");
-    // rester dans la boucle tant que le client est connecté
+    // stay in the loop while client is connected
     while (client.connected())
     {
-       if (client.available()) // Vérifie si le client a envoyé des données
+      if (client.available()) // Check if the client has sent data
+      {
+        String request = client.readStringUntil('\r');
+        Serial.println(request);
+        client.flush();
+
+        if (request.indexOf("/eteindre") != -1)
+        {
+          digitalWrite(LED_PIN, LOW); // Turn off the LED
+          forceLedOn = false;
+        }
+
+        if (request.indexOf("/lirejson") != -1)
+        {
+          if (SPIFFS.exists("/data.json"))
+          {
+            File file = SPIFFS.open("/data.json", FILE_READ);
+            if (!file)
             {
-                String request = client.readStringUntil('\r');
-                Serial.println(request);
-                client.flush();
-
-                if (request.indexOf("/eteindre") != -1)  {
-                    digitalWrite(LED_PIN, LOW); // Éteignez la LED
-                }
-
-                Serial.println("Charge page HTML");
-                client.println("HTTP/1.1 200 OK"); //send new page
-                client.println("Content-Type: text/html");
-
-                client.println();
-                client.print(generateHTMLPage());     
-                client.stop();
+              Serial.println("There was an error opening the file for reading");
+              return;
             }
+
+            Serial.println("Reading JSON data");
+            String jsonData = "";
+            while (file.available())
+            {
+              jsonData += (char)file.read();
+            }
+
+            file.close();
+            Serial.println(jsonData);
+          }
+          else
+          {
+            Serial.println("JSON file does not exist");
+          }
+        }
+
+        if (request.indexOf("/allumer") != -1)
+        {
+          digitalWrite(LED_PIN, HIGH); // Turn on the LED
+          forceLedOn = true;
+        }
+
+        if (sensor1Data.humidity > 60 || forceLedOn)
+        {
+          digitalWrite(LED_PIN, HIGH);
+        }
+        else
+        {
+          digitalWrite(LED_PIN, LOW);
+        }
+
+        Serial.println("Load HTML page");
+        client.println("HTTP/1.1 200 OK"); // send new page
+        client.println("Content-Type: text/html");
+        client.println();
+        client.print(generateHTMLPage());
+        client.stop();
       }
     }
   }
- // End of loop
+}
